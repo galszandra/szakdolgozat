bc5f0006455376e9c8a330affc852775f71628be
Alexandra Gal <galszandra01@gmail.com>
2020-11-19 15:51:50 +0100

    program

---
+++ program/WpfApplication/AddTaskCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scheduler;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Reflection;
+
+namespace WpfApplication
+{
+    public class AddTaskCommand : ICommand
+    {
+        public AddTaskCommand(MainViewModel viewmodel)
+        {
+            mainViewModel = viewmodel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (mainViewModel.TaskTitle == null || TaskTitlesList.Contains(mainViewModel.TaskTitle))
+            {
+                NullParametersList.Add("Cím");
+            }
+
+            if (mainViewModel.TaskPriority == 0)
+            {
+                NullParametersList.Add("Prioritás");
+            }
+
+            if (mainViewModel.TaskDuration == 0 || mainViewModel.TaskDuration > 480)
+            {
+                NullParametersList.Add("Időtartam");
+            }
+
+            if (NullParametersList.Any())
+            {
+                var message = string.Join("\n", NullParametersList);
+                MessageBox.Show(string.Format("A következő érték(ek) hibás(ak):\n{0}", message), "Hibaüzenet", MessageBoxButton.OK, MessageBoxImage.Error);
+                NullParametersList.Clear();
+            }
+
+            else
+            {
+                var task = new Scheduler.Task(mainViewModel.TaskTitle, mainViewModel.TaskPriority, mainViewModel.TaskDuration);
+                mainViewModel.Tasks.Add(task);
+                TaskTitlesList.Add(mainViewModel.TaskTitle);
+
+                NullParametersList.Clear();
+
+               mainViewModel.Titles = DrawChart.DrawTasks(mainViewModel.TaskTitle, mainViewModel.TaskPriority, mainViewModel.TaskDuration, mainViewModel.TaskBars, mainViewModel.Titles);
+
+                MessageBox.Show("Feladat sikeresen hozzáadva! \n" 
+                    + "Cím: " + mainViewModel.TaskTitle + "\n" 
+                    + "Prioritás: " + mainViewModel.TaskPriority + "\n" 
+                    + "Becsült időtartam: " + mainViewModel.TaskDuration + " perc", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                ResetViewModelVariables();
+            }
+        }
+
+        private MainViewModel mainViewModel;
+
+        private List<string> NullParametersList = new List<string>();
+
+        private List<string> TaskTitlesList = new List<string>();
+        private void ResetViewModelVariables()
+        {
+            mainViewModel.TaskTitle = null;
+            mainViewModel.TaskPriority = 0;
+            mainViewModel.TaskDuration = 0;
+        }
+    }
+}
---
+++ program/WpfApplication/OptimizeCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    class OptimizeCommand : ICommand
+    {
+        public OptimizeCommand(MainViewModel viewmodel)
+        {
+            mainViewModel = viewmodel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (mainViewModel.Tasks.Count < 2)
+            {
+                System.Windows.Forms.MessageBox.Show("Még nincs (elég) feladat hozzáadva!", "Hibaüzenet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                int capacity = 480;
+                int n = mainViewModel.Tasks.Count;
+                int[] included = new int[n];
+
+                var orderedList = KnapSack.OrderTasks(mainViewModel.Tasks);
+
+                KnapSack.KnapSackAlgorithm(capacity, orderedList, n, included);
+
+                mainViewModel.Titles = DrawChart.DrawScheduledTasks(mainViewModel.Titles, orderedList, mainViewModel.TaskBars, mainViewModel.Tasks, ExcludedTasks, included);
+            }
+        }
+
+        private List<string> ExcludedTasks = new List<string>();
+
+        private MainViewModel mainViewModel;
+    }
+}
